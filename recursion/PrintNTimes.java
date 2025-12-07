bf9960bec1058b660f8fc259234eea113a5bb313
Dev0711 <111134895+Dev0711@users.noreply.github.com>
2025-12-07 15:42:52 +0530

    Fix PrintNTimes to start counting from 1 instead of 0

--- recursion/PrintNTimes.java
+++ recursion/PrintNTimes.java
@@ -1,15 +1,18 @@
 package recursion;
 
 public class PrintNTimes {
-    static int cnt = 0;
-    static void print(){ // recursive function
-        if(cnt == 6) return; // checking the condition
+    static int cnt = 1;
+
+    static void print() { // recursive function
+        if (cnt == 6)
+            return; // checking the condition
 
         System.out.println(cnt);
-        cnt = cnt +1;
+        cnt = cnt + 1;
         print();
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         print();
     }
 }
