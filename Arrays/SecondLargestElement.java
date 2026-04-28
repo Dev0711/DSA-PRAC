6f737b66625b84450ff60b42cd0adc918e6aee9a
Dev0711 <111134895+Dev0711@users.noreply.github.com>
2026-04-29 00:40:15 +0530

    second largest optimal

--- Arrays/SecondLargestElement.java
+++ Arrays/SecondLargestElement.java
@@ -1,6 +1,7 @@
 package Arrays;
 
 import java.util.Arrays;
+import java.util.function.DoubleToIntFunction;
 
 public class SecondLargestElement {
     public static void getElements(int[] arr, int n) {
@@ -25,6 +26,35 @@ public class SecondLargestElement {
         System.out.println("Second largest is " + large);
     }
 
+    public static void getElemnetsOptimal(int[] arr, int n){
+//        if(n==0 || n ==1){
+//            System.out.println();
+//            return;
+//        }
+
+        int small = Integer.MAX_VALUE;
+        int large = Integer.MIN_VALUE;
+        int second_small = Integer.MAX_VALUE;
+        int second_large = Integer.MIN_VALUE;
+
+        for(int i=0; i< n; i++) {
+            small = Math.min(small, arr[i]);
+            large = Math.max(large, arr[i]);
+        }
+
+        for(int i = 0; i< n; i++){
+            if(arr[i]<second_small && arr[i] != small){
+                second_small = arr[i];
+            }
+            if(arr[i]> second_large && arr[i] != large){
+                second_large = arr[i];
+            }
+        }
+
+        System.out.println("second largest: "+ second_large);
+        System.out.println("second small "+ second_small);
+
+    }
     public static void main(String[] args) {
         // Initialize the array with elements
         int[] arr = {1, 2, 4, 6, 7, 5};
@@ -33,7 +63,8 @@ public class SecondLargestElement {
         int n = arr.length;
 
         // Call the method to find and print the second smallest and second largest elements
-        SecondLargestElement.getElements(arr, n);
+//        SecondLargestElement.getElements(arr, n);
+        SecondLargestElement.getElemnetsOptimal(arr, n);
     }
 
 }
